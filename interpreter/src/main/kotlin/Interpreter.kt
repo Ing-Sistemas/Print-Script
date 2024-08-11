4e2b70337b9410364503da50e420f8477f0d5878
agussoul <aalma@mail.austral.edu.ar>
2024-08-11 17:01:23 -0300

    fix's and methods on Interpreter

--- interpreter/src/main/kotlin/Interpreter.kt
+++ interpreter/src/main/kotlin/Interpreter.kt
@@ -6,19 +6,33 @@ class Interpreter {
     fun interpret(node: ASTNode): Any {
         return when (node) {
             is AssignmentNode -> visitAssignment(node)
-            is CallNode -> TODO("Implement")
-            is IdentifierNode -> TODO("Implement")
-            is LeafNode -> TODO("Implement")
-            is LiteralNode -> TODO("Implement")
-            is ProgramNode -> TODO("Implement")
-            is StatementNode -> TODO("Implement")
-            is TypeDeclarationNode -> TODO("Implement")
-            is VariableDeclarationNode -> TODO("Implement")
+            is LeafNode -> visitLeaf(node)
+            is ProgramNode -> visitProgram(node)
+            is VariableDeclarationNode -> visitVariableDeclaration(node)
+            is StatementNode -> visitStatement(node)
+            is TypeDeclarationNode -> visitTypeDeclaration(node)
+            is LiteralNode -> visitLiteral(node)
             else -> throw IllegalArgumentException("Unknown node type: ${node.javaClass}")
         }
     }
 
-    private fun visitAssignment(node: AssignmentNode) : Any {
-        TODO("Implement")
+    private fun visitLeaf(node: LeafNode): Any {
+        return node.getToken().getValue()
+    }
+
+    private fun visitProgram(node: ProgramNode): Any {
+        //TODO visitar todos los children y hacerles a cada uno interpret(child)
+    }
+
+    private fun visitVariableDeclaration(node: VariableDeclarationNode): Any {
+        //asumo q lo guardamos: left: nombre, right: valor TODO checkear esto
+        val variableName = (node.getChildren()[0] as LeafNode).getToken().getValue()
+        val value = interpret(node.getChildren()[1])
+        storage[variableName] = value
+        return value
+    }
+
+    private fun visitLiteral(node: LiteralNode): Any {
+        return node.getToken().getValue()
     }
 }
