52e005739d035c70e72ec30af0de1f0436b8d243
agussoul <aalma@mail.austral.edu.ar>
2024-08-11 21:03:03 -0300

    start on semantic

--- parser/src/main/kotlin/SemanticAnalyzer.kt
+++ parser/src/main/kotlin/SemanticAnalyzer.kt
@@ -1,4 +1,42 @@
-package org.example.parser
+package org.example
+
+import Token
 
 class SemanticAnalyzer {
+    fun analyze(node: ASTNode) {
+        when (node) {
+            //is ProgramNode -> TODO()
+            is AssignmentNode -> analyzeAssignment(node)
+            is BinaryNode -> analyzeBinary(node)
+
+
+            else -> throw Exception("Unknown node type: ${node.javaClass}")
+        }
+    }
+
+    private fun analyzeAssignment(node: AssignmentNode) {
+        val identifier = node.getIdentifierNode()
+        val literal = node.getLiteralNode()
+
+        compareTypes(identifier.getToken(), literal.getToken())
+    }
+
+    private fun analyzeBinary(node: BinaryNode) {
+        val left = node.getLeft()
+        val right = node.getRight()
+
+        compareTypes(left.getToken(), right.getToken())
+    }
+
+
+
+
+
+    private fun compareTypes(left: Token, right: Token) {
+        if (left.getType() != right.getType()) {
+            throw Exception("Type mismatch: ${left.getType()} != ${right.getType()}")
+        }
+    }
+
+
 }
