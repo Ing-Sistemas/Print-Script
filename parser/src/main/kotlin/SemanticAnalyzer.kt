33e0347df56acff48d2c86e96a113f9ae2ecf8ca
agussoul <aalma@mail.austral.edu.ar>
2024-08-12 17:30:41 -0300

    added strategy for operators and implemented some of the node's semantic ops

--- parser/src/main/kotlin/SemanticAnalyzer.kt
+++ parser/src/main/kotlin/SemanticAnalyzer.kt
@@ -1,10 +1,12 @@
-package org.example
-
-import Token
+import org.example.ASTNode
+import org.example.AssignmentNode
+import org.example.BinaryNode
+import org.example.token.TokenType
+import stategy.operators.DivideTokenStrategy
 
 class SemanticAnalyzer {
-    fun analyze(node: ASTNode) {
-        when (node) {
+    fun analyze(node: ASTNode): ASTNode {
+        return when (node) {
             //is ProgramNode -> TODO()
             is AssignmentNode -> analyzeAssignment(node)
             is BinaryNode -> analyzeBinary(node)
@@ -14,20 +16,32 @@ class SemanticAnalyzer {
         }
     }
 
-    private fun analyzeAssignment(node: AssignmentNode) {
+    private fun analyzeAssignment(node: AssignmentNode): ASTNode {
         val identifier = node.getIdentifierNode()
         val literal = node.getLiteralNode()
 
         compareTypes(identifier.getToken(), literal.getToken())
+        return node
     }
 
-    private fun analyzeBinary(node: BinaryNode) {
+    private fun analyzeBinary(node: BinaryNode): ASTNode {
         val left = node.getLeft()
         val right = node.getRight()
 
         compareTypes(left.getToken(), right.getToken())
+        checkOperator(node.getToken())
+        return node
     }
 
+    private fun checkOperator(operator: Token) {
+        if (operator.getValue() != "+" &&
+            operator.getValue() != "-" &&
+            operator.getValue() != "*" &&
+            operator.getValue() != "/"
+        ) {
+            throw Exception("Invalid operator: ${operator.getValue()}")
+        }
+    }
 
 
 
