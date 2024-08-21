0c67c37cd79c9f30e930c22412184dda450ab34b
Felipe Germano <fgermano@mail.austral.edu.ar>
2024-08-21 00:23:46 -0300

    some tests

--- interpreter/src/test/kotlin/InterpreterTests.kt
+++ interpreter/src/test/kotlin/InterpreterTests.kt
@@ -7,38 +7,39 @@ class InterpreterTests {
 
     // let a: number = 4;
 
-//    val keyword = Token(TokenType.KEYWORD, "let")
-//    val type = Token(TokenType.NUMBER_TYPE, "number")
-//    val typeNode = TypeDeclarationNode(type, 0, 10)
-//    val tokenIdentifier = Token(TokenType.IDENTIFIER, "a")
-//    val identifier = IdentifierNode(tokenIdentifier, 0, 10)
-//
-//    val valueToken = Token(TokenType.LITERAL_NUMBER, "4")
-//    val valueNode = LiteralNode(valueToken, 0, 10)
-//
-//    val assignToken = Token(TokenType.ASSIGNMENT, "=")
-//    val assignment = AssignmentNode(assignToken, identifier, valueNode, 0, 10)
-//
-//    val firstExample = VariableDeclarationNode(keyword, 0, 10, typeNode, assignment)
-//
-//    val statement = StatementNode(firstExample, TokenType.SEMICOLON, 0, 10)
-//
-//    val statementListed = listOf<ASTNode>(statement)
-//    val myProgram = ProgramNode(0, 10, statementListed)
-//
+    val aKeyword = Token(TokenType.KEYWORD, "let")
+    val aType = Token(TokenType.NUMBER_TYPE, "number")
+    val aTypeNode = TypeDeclarationNode(aType.getValue(), 0, 10)
+    val aTokenIdentifier = Token(TokenType.IDENTIFIER, "a")
+    val aIdentifier = IdentifierNode(aTokenIdentifier.getValue(), 0, 10)
+
+    val aValueToken = Token(TokenType.LITERAL_NUMBER, "4")
+    val aValueNode = LiteralNode(aValueToken.getValue(), aValueToken.getType().toString(),0, 92)
+
+    val aAssignToken = Token(TokenType.ASSIGNMENT, "=")
+    val aAssignment = AssignmentNode(aAssignToken.getValue(), aIdentifier, aValueNode, 0, 15)
+
+    val firstExample = VariableDeclarationNode(aKeyword.getValue(), aTypeNode, aAssignment, 0,22)
+
+    val statement = StatementNode(firstExample,0, 19)
+
+    val statementListed = listOf<ASTNode>(statement)
+    val myProgram = ProgramNode(0, 10, statementListed)
+    val storage = interpreter.getStorage()
+
 //    // println("hello world!")
-//
-//    val printCall = Token(TokenType.CALL, "println" )
-//    val argument = Token(TokenType.LITERAL_STRING, "hello world!")
-//    val literalNode2 = LiteralNode(argument, 0, 12)
-//    val listedArguments = listOf<ASTNode>(literalNode2)
-//    val caller = CallNode(printCall, listedArguments, 0, 10)
-//
-//    val statement2 = StatementNode(caller, TokenType.CLOSING_PARENS, 0, 22)
-//
-//    val statementeListed2 = listOf<ASTNode>(statement2)
-//    val myProgram2 = ProgramNode(0, 55, statementeListed2)
-//
+
+    val printCall = Token(TokenType.CALL, "println" )
+    val printArgument = Token(TokenType.LITERAL_STRING, "'hello world!'")
+    val literalPrintNode = LiteralNode(printArgument.getValue(), printArgument.getType().toString(), 0,12)
+    val listedArgumentsForPrint = listOf<ASTNode>(literalPrintNode)
+    val printCaller = CallNode(printCall.getValue(), listedArgumentsForPrint, 0, 10)
+
+    val statement2 = StatementNode(printCaller,0, 52)
+
+    val statementListed2 = listOf<ASTNode>(statement2)
+    val myProgram2 = ProgramNode(0, 55, statementListed2)
+
 
     @Test
     fun testEmptyStorage(){
@@ -48,8 +49,7 @@ class InterpreterTests {
 
     @Test
     fun testStorageVarDeclaration(){
-        val storage = interpreter.getStorage()
-        //interpreter.interpret(myProgram)
+        interpreter.interpret(myProgram)
         assert(storage.isNotEmpty())
         assert(storage.containsValue(4))
         assert(storage.containsKey("a"))
@@ -57,9 +57,6 @@ class InterpreterTests {
 
     @Test
     fun testPrint(){
-        //  val interpreted = interpreter.interpret(myProgram2)
-        //println("[$interpreted]88")
-        //  println(interpreter.interpret(myProgram2))
-        //assert(interpreter.interpret(myProgram2) == "hello world!")
+        assert(interpreter.interpret(myProgram2) == println("'hello world!'"))
     }
 }
