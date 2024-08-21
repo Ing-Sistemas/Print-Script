0fb2ef5383ad54b299dd8c195f62dd962b8963f6
agussoul <aalma@mail.austral.edu.ar>
2024-08-21 11:30:07 -0300

    moved "testing" from Main into TestClass for Runner

--- runner/src/main/kotlin/Main.kt
+++ runner/src/main/kotlin/Main.kt
@@ -1,20 +1,5 @@
 package org.example
 
 fun main() {
-    val runner = Runner()
-
-    val code1 = "let a: number = 5; println(a + a);"
-    val code2 = "let a: string = 'ast'; println('Result: ' + a);"
-    val code3 = "let a: number = 5; println(3 + 5 + 6);"
-    val code4 = "let a: number = 12; let c: number = a;"
-    val code5 = "let name: string = 'Joe'; let lastName: string = 'Doe'; println(name + ' ' + lastName);"
-    val code6 = "let a: number = 12; let b: number = 4; let c: number = a / b; println('Result: ' + c);"
-    val code7 = "let a: number = 12; let b: number = 4; a = a / b; println('Result: ' + a);"
-    //invalid cases
-    val code8 = "let a: number = '12';"
-    val code9 = "let a: string = 'hello';"
-
-    runner.run(code8)
-
 
 }
---
+++ runner/src/test/kotlin/RunnerTests.kt
@@ -0,0 +1,63 @@
+import org.example.Runner
+import org.junit.jupiter.api.Test
+import org.junit.jupiter.api.assertThrows
+import kotlin.test.assertEquals
+
+class RunnerTests {
+    val runner = Runner()
+
+    @Test
+    fun `test case`() {
+        val code = "let a: number = 5; println(a + a);"
+        runner.run(code)
+    }
+
+    @Test
+    fun `test case 2`() {
+        val code2 = "let a: string = 'ast'; println('Result: ' + a);"
+        runner.run(code2)
+    }
+
+    @Test
+    fun `test case 3`() {
+        val code3 = "let a: number = 5; println(3 + 5 + 6);"
+        runner.run(code3)
+    }
+
+    @Test
+    fun `test case 4`() {
+        val code4 = "let a: number = 12; let c: number = a;"
+        runner.run(code4)
+    }
+
+    @Test
+    fun `test case 5`() {
+        val code5 = "let name: string = 'Joe'; let lastName: string = 'Doe'; println(name + ' ' + lastName);"
+        runner.run(code5)
+    }
+
+    @Test
+    fun `test case 6`() {
+        val code6 = "let a: number = 12; let b: number = 4; let c: number = a / b; println('Result: ' + c);"
+        runner.run(code6)
+    }
+
+    @Test
+    fun `test case 7`() {
+        val code7 = "let a: number = 12; let b: number = 4; a = a / b; println('Result: ' + a);"
+        runner.run(code7)
+    }
+
+    // Invalid cases
+    @Test
+    fun `test case 8`() {
+        val code8 = "let a: number = '12';"
+        assertThrows<Exception> { runner.run(code8) }
+    }
+
+    @Test
+    fun `test case 9`() {
+        val code9 = "let a: string = 'hello';"
+        runner.run(code9)
+    }
+}
