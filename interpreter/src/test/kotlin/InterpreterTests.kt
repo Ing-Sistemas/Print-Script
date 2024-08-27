fa46b690b5be115fdd54f68ddc0609a79c4b5c9f
nicolasberretta <neberretta@mail.austral.edu.ar>
2024-08-27 17:49:35 -0300

    fixed pre-commit

--- interpreter/src/test/kotlin/InterpreterTests.kt
+++ interpreter/src/test/kotlin/InterpreterTests.kt
@@ -14,49 +14,45 @@ class InterpreterTests {
     val aIdentifier = IdentifierNode(aTokenIdentifier.getValue(), 0, 10)
 
     val aValueToken = Token(TokenType.LITERAL_NUMBER, "4")
-    val aValueNode = LiteralNode(aValueToken.getValue(), aValueToken.getType().toString(),0, 92)
+    val aValueNode = LiteralNode(aValueToken.getValue(), aValueToken.getType().toString(), 0, 92)
 
     val aAssignToken = Token(TokenType.ASSIGNMENT, "=")
     val aAssignment = AssignmentNode(aAssignToken.getValue(), aIdentifier, aValueNode, 0, 15)
 
-    val firstExample = VariableDeclarationNode(aKeyword.getValue(), aTypeNode, aAssignment, 0,22)
+    val firstExample = VariableDeclarationNode(aKeyword.getValue(), aTypeNode, aAssignment, 0, 22)
 
-    val statement = StatementNode(firstExample,0, 19)
+    val statement = StatementNode(firstExample, 0, 19)
 
     val statementListed = listOf<ASTNode>(statement)
     val myProgram = ProgramNode(0, 10, statementListed)
     val storage = interpreter.getStorage()
 
-
-
     // println("hello world!")
 
-
-    val printCall = Token(TokenType.CALL, "println" )
+    val printCall = Token(TokenType.CALL, "println")
     val printArgument = Token(TokenType.LITERAL_STRING, "'hello world!'")
-    val literalPrintNode = LiteralNode(printArgument.getValue(), printArgument.getType().toString(), 0,12)
+    val literalPrintNode = LiteralNode(printArgument.getValue(), printArgument.getType().toString(), 0, 12)
     val listedArgumentsForPrint = listOf<ASTNode>(literalPrintNode)
     val printCaller = CallNode(printCall.getValue(), listedArgumentsForPrint, 0, 10)
 
-    val statement2 = StatementNode(printCaller,0, 52)
+    val statement2 = StatementNode(printCaller, 0, 52)
 
     val statementListed2 = listOf<ASTNode>(statement2)
     val myProgram2 = ProgramNode(0, 55, statementListed2)
 
     @Test
-    fun testEmptyStorage(){
+    fun testEmptyStorage() {
         val emptyStorage = interpreter.getStorage()
         assert(emptyStorage.isEmpty())
     }
 
     @Test
-    fun testStorageVarDeclaration(){
+    fun testStorageVarDeclaration() {
         interpreter.interpret(myProgram)
         assert(storage.isNotEmpty())
         assert(storage.containsValue(4))
         assert(storage.containsKey("a"))
     }
-
 
 //    @Test
 //    fun testPrint(){
