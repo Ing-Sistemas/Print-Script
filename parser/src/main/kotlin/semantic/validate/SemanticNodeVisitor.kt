49cc8988c32a684330da7b643568ab2cb8f8a7e6
agussoul <aalma@mail.austral.edu.ar>
2024-08-27 18:02:02 -0300

    pr conflicts fix?

--- parser/src/main/kotlin/semantic/validate/SemanticNodeVisitor.kt
+++ parser/src/main/kotlin/semantic/validate/SemanticNodeVisitor.kt
@@ -1,124 +1,41 @@
-package org.example.parser.semantic.validate
+package org.example.parser.semantic
 
 import org.example.*
-import org.example.parser.semantic.ResultInformation
 
-class SemanticNodeVisitor: Visitor<ResultInformation> {
-    //TODO("all exception -> Result")
-    private val storage = mutableMapOf<String, Any>()
+class SemanticNodeVisitor(
+    private val typeCheck: TypeCheck,
+    private val storageManager: StorageManager,
+    private val operationCheck: OperationCheck,
+    private val resultFactory: ResultFactory
+) : Visitor<ResultInformation> {
 
     override fun visit(programNode: ProgramNode): ResultInformation {
-        val errors = mutableListOf<String>()
-        programNode.getChildren().forEach {
-            val result = it.accept(this)
-            result.getErrors().forEach { errors.add(it) }
-        }
-        return ResultInformation(null, null, errors)
+        val errors = programNode.getChildren().flatMap { it.accept(this).getErrors() }
+        return resultFactory.create(null, null, errors)
     }
 
     override fun visit(literalNode: LiteralNode): ResultInformation {
-        return ResultInformation(literalNode.getValue(), literalNode.getType(), emptyList())
+        return resultFactory.create(literalNode.getValue(), literalNode.getType())
     }
 
     override fun visit(typeDeclaration: TypeDeclarationNode): ResultInformation {
-        return ResultInformation(null, typeDeclaration.getValue(), emptyList())
+        return resultFactory.create(null, typeDeclaration.getValue())
     }
 
     override fun visit(identifierNode: IdentifierNode): ResultInformation {
-        val variableName = identifierNode.getValue()
-        if (variableName in storage) {
-            val value = storage[variableName].toString()
-            val type = storage[variableName]?.let { getTypeForVar(it) }
-            return ResultInformation(variableName, type, emptyList())
-        }
-        return ResultInformation(variableName, null, emptyList())
-    }
-
-    private fun getTypeForVar(value: Any): String {
-        return when (value) {
-            is Int -> "LITERAL_NUMBER"
-            is String -> "LITERAL_STRING"
-            else -> "UNKNOWN"
-        }
+        return storageManager.getIdentifierResult(identifierNode)
     }
 
     override fun visit(variableDeclarationNode: VariableDeclarationNode): ResultInformation {
-        val typeDeclaration = variableDeclarationNode.getTypeDeclaration().getValue() //'string' o 'number'
-        val identifierName = variableDeclarationNode.getAssignment().getIdentifierNode().getValue()
-        if (identifierName in storage) return ResultInformation(null, null, listOf("Variable name already in use"))
-        val assignmentValue = variableDeclarationNode.getAssignment().accept(this)
-        if (assignmentValue.getErrors().isNotEmpty()) return ResultInformation(null, null, assignmentValue.getErrors())
-        val typeMatchingAssignmentSide = if (assignmentValue.getType() == "LITERAL_NUMBER") "number" else "string"
-        if (typeDeclaration != typeMatchingAssignmentSide) return ResultInformation(null, null, listOf("Type mismatch for variable declaration"))
-        return ResultInformation(assignmentValue.getValue(), assignmentValue.getType(), emptyList())
+        return typeCheck.checkVariableDeclaration(variableDeclarationNode, this)
     }
 
     override fun visit(binaryNode: BinaryNode): ResultInformation {
-        val left = binaryNode.getLeft().accept(this)
-        val right = binaryNode.getRight().accept(this)
-        return performOperation(left, right, binaryNode.getValue())
-    }
-
-    private fun performOperation(left: ResultInformation, right: ResultInformation, operator: String): ResultInformation {
-        val leftValue = getIntValue(left.getValue().toString(), storage)
-        val rightValue = getIntValue(right.getValue().toString(), storage)
-        return when (operator) {
-            "+" -> ResultInformation(left.getValue() + right.getValue(), null, emptyList())
-            "-" -> {
-                if (left.getType() == right.getType()) {
-                    val operation = left.getValue()!!.toInt() - right.getValue()!!.toInt()
-                    ResultInformation(operation.toString(), left.getType(), emptyList())
-                } else {
-                    ResultInformation(null, null, listOf("Type mismatch for subtraction"))
-                }
-            }
-            "/" -> {
-                if (left.getType() == right.getType()) {
-                    val operation = leftValue!! / rightValue!!
-                    ResultInformation(operation.toString(), "LITERAL_NUMBER", emptyList())
-                } else {
-                    ResultInformation(null, null, listOf("Type mismatch for division"))
-                }
-            }
-            "*" -> {
-                if (left.getType() == right.getType()) {
-                    val operation = leftValue!! * rightValue!!
-                    ResultInformation(operation.toString(), "LITERAL_NUMBER", emptyList())
-                } else {
-                    ResultInformation(null, null, listOf("Type mismatch for multiplication"))
-                }
-            }
-            else -> ResultInformation(null, null, listOf("Invalid operator specified"))
-        }
-    }
-
-    private fun getIntValue(key: String, map: Map<String, Any>): Int? {
-        val value = map[key]
-        return when (value) {
-            is Int -> value
-            is String -> value.toIntOrNull() // Convert String to Int if possible
-            else -> null // Return null if it's neither an Int nor a convertible String
-        }
+        return operationCheck.checkBinaryOperation(binaryNode, this)
     }
 
     override fun visit(assignmentNode: AssignmentNode): ResultInformation {
-        val identifierNodeName = assignmentNode.getIdentifierNode().accept(this)
-        val valueNode = assignmentNode.getValueNode().accept(this)
-//        if (storage[valueNode] != null) {
-//            return ResultInformation(storage[valueNode], valueNode.getType(), emptyList())
-//        }
-        // todo ver si el valueNode esta como key en storage.
-        val value = tryToInt(valueNode.getValue().toString())
-        storage[identifierNodeName.getValue().toString()] = value
-        return ResultInformation(valueNode.getValue(), valueNode.getType(), identifierNodeName.getErrors() + valueNode.getErrors())
-    }
-
-    private fun tryToInt(value: String): Any {
-        return try {
-            value.toInt()
-        } catch (e: Exception) {
-            value
-        }
+        return storageManager.handleAssignment(assignmentNode, this)
     }
 
     override fun visit(statementNode: StatementNode): ResultInformation {
@@ -130,6 +47,6 @@ class SemanticNodeVisitor: Visitor<ResultInformation> {
     }
 
     override fun visit(callNode: CallNode): ResultInformation {
-        return ResultInformation(null, null, emptyList())
+        return resultFactory.create(null, null)
     }
 }
