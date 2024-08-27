2641c1891b6242f4dcf3da3f209bf00e7a40f7c9
agussoul <aalma@mail.austral.edu.ar>
2024-08-27 15:22:38 -0300

    moved visitor file, refactored it

--- parser/src/main/kotlin/semantic/SemanticAnalyzer.kt
+++ parser/src/main/kotlin/semantic/SemanticAnalyzer.kt
@@ -1,12 +1,17 @@
 package semantic
 
 import org.example.*
-import org.example.parser.semantic.validate.SemanticNodeVisitor
+import org.example.parser.semantic.*
 
 
 class SemanticAnalyzer {
+    private val resultFactory = ResultFactory()
+    private val typeCheck = TypeCheck(resultFactory)
+    private val storageManager = StorageManager(resultFactory)
+    private val operationCheck = OperationCheck(resultFactory, storageManager)
+
     fun analyze(node: ASTNode): ASTNode {
-        val result = SemanticNodeVisitor().visit(node as ProgramNode)
+        val result = SemanticNodeVisitor(typeCheck, storageManager, operationCheck, resultFactory).visit(node as ProgramNode)
         if (result.getErrors().isNotEmpty()) {
             throw Exception("Semantic error: ${result.getErrors()}")
         }
