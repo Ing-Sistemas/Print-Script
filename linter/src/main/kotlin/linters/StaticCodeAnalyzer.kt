0f3744e4e78238629c40e7a95412b2fe2761b455
Felipe Germano <fgermano@mail.austral.edu.ar>
2024-08-28 11:02:44 -0300

    changed name linter -> static code analyzer for no confusion

--- linter/src/main/kotlin/linters/StaticCodeAnalyzer.kt
+++ linter/src/main/kotlin/linters/StaticCodeAnalyzer.kt
@@ -4,14 +4,14 @@ import Token
 import configurations.Configuration
 import interfaces.Linter
 
-class Linter(configuration: Configuration) {
+class StaticCodeAnalyzer(configuration: Configuration) {
 
     private val linters: List<Linter> = listOf(
         IdentifierCaseLinter(configuration.caseConfiguration),
         if (configuration.restrictPrintln) PrinterRestrictions() else null,
     ).filterNotNull()
 
-    fun lint(tokens: List<Token>): List<String> {
+    fun analyze(tokens: List<Token>): List<String> {
         val allErrors = mutableListOf<String>()
         for (linter in linters) {
             allErrors.add(linter.lint(tokens).toString())
