6c1c54aae32f416a1a4765a6ea360786dfe35c3c
agussoul <aalma@mail.austral.edu.ar>
2024-08-29 17:32:21 -0300

    created config, example and reader

--- formatter/src/main/kotlin/CodeFormatter.kt
+++ formatter/src/main/kotlin/CodeFormatter.kt
@@ -1,6 +1,20 @@
 package org.example
 
+import org.example.config.FormatterConfig
+import org.example.config.JsonReader
+
 class CodeFormatter {
     // lee el json de la config y dependiendo de que value
     // encuentra entra en un if o en el else (pq muchas son booleanas)
+    // write into file (override previous)
+    // todo leer el .txt con el codigo a formatear
+    private val config = JsonReader().convertJsonIntoConfig("config.json")
+
+    fun format(code: String, config: FormatterConfig): String {
+        val formattedCode = StringBuilder()
+
+        // todo
+
+        return formattedCode.toString()
+    }
 }
