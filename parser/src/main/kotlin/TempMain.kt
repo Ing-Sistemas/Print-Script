357198396446e818713ff3375a1205d6921ebf65
agussoul <aalma@mail.austral.edu.ar>
2024-08-29 14:31:19 -0300

    deleted unused classes and implemented tests for coverage

--- parser/src/main/kotlin/TempMain.kt
+++
@@ -1,4 +0,0 @@
-package org.example.parser
-
-fun main() {
-}
--- parser/src/test/kotlin/ParserTests.kt
+++ parser/src/test/kotlin/ParserTests.kt
@@ -1,6 +1,11 @@
+import com.google.gson.GsonBuilder
+import com.google.gson.ToNumberPolicy
 import org.example.parser.Parser
 import org.example.token.TokenType.*
 import org.junit.jupiter.api.Test
+import org.junit.jupiter.api.assertDoesNotThrow
+import org.junit.jupiter.api.assertThrows
+import kotlin.test.assertEquals
 
 class ParserTests {
 
@@ -78,5 +83,57 @@ class ParserTests {
         )
         val resultAst = Parser().parse(tokens)
         assert(jsonTester.testJson(resultAst, "binaryNode.json"))
+    }
+
+    @Test
+    fun `Invalid Variable Declaration Analysis`() {
+        val jsonTester = JsonTester()
+        val tokens = listOf<Token>(
+            Token(KEYWORD, "let"),
+            Token(IDENTIFIER, "myVar"),
+            Token(COLON, ":"),
+            Token(STRING_TYPE, "number"),
+            Token(ASSIGNMENT, "="),
+            Token(LITERAL_STRING, "'hola'"),
+            Token(SEMICOLON, ";"),
+        )
+        assertThrows<Exception> { Parser().parse(tokens) }
+    }
+
+    @Test
+    fun `Invalid Syntactic Expression, missing semicolon`() {
+        val jsonTester = JsonTester()
+        val tokens = listOf<Token>(
+            Token(CALL, "println"),
+            Token(OPENING_PARENS, "("),
+            Token(LITERAL_STRING, "2"),
+            Token(PLUS_OPERATOR, "+"),
+            Token(LITERAL_NUMBER, "9"),
+            Token(CLOSING_PARENS, ")"),
+        )
+        assertThrows<Exception> { Parser().parse(tokens) }
+    }
+
+    // todo add operation tests for + - * /
+
+    @Test
+    fun `Crate Operations For Every Operator`() {
+
+        val tokens = listOf<Token>(
+            Token(CALL, "println"),
+            Token(OPENING_PARENS, "("),
+            Token(LITERAL_STRING, "2"),
+            Token(PLUS_OPERATOR, "+"),
+            Token(LITERAL_NUMBER, "9"),
+            Token(MULTIPLY_OPERATOR, "*"),
+            Token(LITERAL_NUMBER, "2"),
+            Token(DIVIDE_OPERATOR, "/"),
+            Token(LITERAL_NUMBER, "2"),
+            Token(MINUS_OPERATOR, "-"),
+            Token(LITERAL_NUMBER, "2"),
+            Token(CLOSING_PARENS, ")"),
+            Token(SEMICOLON, ";"),
+        )
+        assertDoesNotThrow { Parser().parse(tokens) }
     }
 }
