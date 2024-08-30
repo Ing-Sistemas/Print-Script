83a98d587819a9d2067bb905e2d912ddf5e5e362
agussoul <aalma@mail.austral.edu.ar>
2024-08-30 20:10:23 -0300

    finished formatter and created a main to test it

--- formatter/src/main/kotlin/config/JsonReader.kt
+++ formatter/src/main/kotlin/config/JsonReader.kt
@@ -4,7 +4,7 @@ import com.google.gson.Gson
 import java.io.File
 
 class JsonReader {
-    fun convertJsonIntoConfig(filepath: String): FormatterConfig {
+    fun convertJsonIntoFormatterConfig(filepath: String): FormatterConfig {
         val gson = Gson()
         val jsonString = File(filepath).readText()
         return gson.fromJson(jsonString, FormatterConfig::class.java)
