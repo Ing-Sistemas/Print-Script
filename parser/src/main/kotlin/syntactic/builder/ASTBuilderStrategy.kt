a866352892da89256dd1fc897485d17e167e8da9
nicolasberretta <neberretta@mail.austral.edu.ar>
2024-09-02 23:37:39 -0300

    added new interface for ast builders

--- parser/src/main/kotlin/syntactic/builder/ASTBuilderStrategy.kt
+++ parser/src/main/kotlin/syntactic/builder/ASTBuilderStrategy.kt
@@ -1,13 +1,12 @@
 package org.example.parser.syntactic.builder
 
+import ASTNode
 import Token
 
 interface ASTBuilderStrategy {
-    /**
-     * builders look for the index of the given token and start building the AST according to a predefined structure.
-     *
-     * This structure is defined using the indexes relative to the given token
-     */
 
     fun build(token: Token, tokens: List<Token>): ASTNode
+
+    fun isValidStruct(tokens: List<Token>): Boolean
+
 }
