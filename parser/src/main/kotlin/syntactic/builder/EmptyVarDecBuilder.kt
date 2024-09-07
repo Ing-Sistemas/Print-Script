b0e797366016b4cf33546bbc5a3a5eccbc030799
nicolasberretta <neberretta@mail.austral.edu.ar>
2024-09-07 20:21:04 -0300

    add new builders

---
+++ parser/src/main/kotlin/syntactic/builder/EmptyVarDecBuilder.kt
@@ -0,0 +1,27 @@
+package org.example.parser.syntactic.builder
+
+import ASTNode
+import Token
+import org.example.token.TokenType.*
+
+class EmptyVarDecBuilder: ASTBuilderStrategy {
+    private val expectedSize = 5
+    private val expectedStruct= listOf(
+        KEYWORD,
+        IDENTIFIER,
+        COLON,
+        TYPE, // STRING_TYPE or NUMBER_TYPE
+    )
+
+    override fun build(tokens: List<Token>): ASTNode {
+        TODO("Not yet implemented")
+    }
+
+    override fun isValidStruct(tokens: List<Token>): Boolean {
+        TODO("Not yet implemented")
+    }
+
+    override fun respectsExpectedSize(size: Int): Boolean {
+        TODO("Not yet implemented")
+    }
+}
---
+++ parser/src/main/kotlin/syntactic/builder/ExpressionBuilder.kt
@@ -0,0 +1,18 @@
+package org.example.parser.syntactic.builder
+
+import ASTNode
+import Token
+
+class ExpressionBuilder: ASTBuilderStrategy {
+    override fun build(tokens: List<Token>): ASTNode {
+        TODO("Not yet implemented")
+    }
+
+    override fun isValidStruct(tokens: List<Token>): Boolean {
+        TODO("Not yet implemented")
+    }
+
+    override fun respectsExpectedSize(size: Int): Boolean {
+        TODO("Not yet implemented")
+    }
+}
