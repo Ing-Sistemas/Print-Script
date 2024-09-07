133f4bc10952e9cac88ddbe517766bd11b2f99c6
= <aalma@mail.austral.edu.ar>
2024-09-07 19:54:37 -0300

    commented all syntax files and added semantic specific tests

--- ast/src/main/kotlin/jsonAdapter/ASTNodeDeserializer.kt
+++ ast/src/main/kotlin/jsonAdapter/ASTNodeDeserializer.kt
@@ -1,12 +1,12 @@
-
-
-import com.google.gson.*
-import org.example.ASTNode
-import java.lang.reflect.Type
-
-class ASTNodeDeserializer() : JsonDeserializer<ASTNode> {
-    override fun deserialize(p0: JsonElement?, p1: Type?, p2: JsonDeserializationContext?): ASTNode {
-        val gson = Gson()
-        TODO()
-    }
-}
+//
+//
+//import com.google.gson.*
+//import org.example.ASTNode
+//import java.lang.reflect.Type
+//
+//class ASTNodeDeserializer() : JsonDeserializer<ASTNode> {
+//    override fun deserialize(p0: JsonElement?, p1: Type?, p2: JsonDeserializationContext?): ASTNode {
+//        val gson = Gson()
+//        TODO()
+//    }
+//}
