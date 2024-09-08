e49086fa7a7d01d177d77e76c5f5b61bfd4f477d
Felipe Germano <fgermano@mail.austral.edu.ar>
2024-09-07 23:48:07 -0300

    addded configuration for 1.1 not working yet but started

--- interpreter/src/main/kotlin/output/Conditional.kt
+++ interpreter/src/main/kotlin/output/Conditional.kt
@@ -4,6 +4,9 @@ import interfaces.OutPut
 
 class Conditional : OutPut {
     override fun output(code: Any) {
-        TODO("Not yet implemented")
+        if (code == true) {
+            // interpret expression in if¿?
+            TODO()
+        }
     }
 }
