ff9a44345189842e8ccf54ae200cd0dc7f2a0654
nicolasberretta <neberretta@mail.austral.edu.ar>
2024-09-10 11:12:57 -0300

    removed comments with old impl

--- parser/src/main/kotlin/syntactic/builder/FunctionCallBuilder.kt
+++ parser/src/main/kotlin/syntactic/builder/FunctionCallBuilder.kt
@@ -9,8 +9,11 @@ class FunctionCallBuilder : ASTBuilderStrategy {
     private val expectedStruct= listOf(
         CALL,
         OPENING_PARENS,
-        CLOSING_PARENS
+        CLOSING_PARENS,
+        OPENING_CURLY_BRACKS,
+        CLOSING_CURLY_BRACKS,
     )
+
     override fun build(tokens: List<Token>): ASTNode {
         TODO("Not yet implemented")
     }
@@ -20,46 +23,3 @@ class FunctionCallBuilder : ASTBuilderStrategy {
     }
 
 }
-//override fun build(token: Token, tokens: List<Token>): ASTNode {
-//        val tokenPos = tokens.indexOf(token)
-//        if (!hasParens(tokenPos, tokens)) {
-//            error("Call parenthesis not found")
-//        }
-//        val args = mutableListOf<ASTNode>()
-//        val tokensArgs = tokens.subList(tokenPos + 2, tokens.lastIndex - 1) // grabs all the arguments between the () //excluding them
-//        if (tokensArgs.size > 1 && isOperator(tokensArgs[1])) {
-//            val ast = BinaryExpressionBuilder().build(tokensArgs[1], tokensArgs)
-//            args.add(ast)
-//        } else {
-//            when (val type = tokens[tokenPos + 2].getType()) {
-//                // checks the following token inside the ( ), therefore it builds the args list
-//                LITERAL_NUMBER -> args.add(LiteralNode(tokens[tokenPos + 2].getValue(), type.name, 0, 0))
-//                LITERAL_STRING -> args.add(LiteralNode(tokens[tokenPos + 2].getValue(), type.name, 0, 0))
-//                IDENTIFIER -> args.add(IdentifierNode(tokens[tokenPos + 2].getValue(), 0, 0))
-//                else -> error("Unexpected token type")
-//            }
-//        }
-//
-//        return CallNode(token.getValue(), args, 0, 0)
-//    }
-//
-//    override fun isValidStruct(tokens: List<Token>): Boolean {
-//        TODO("Not yet implemented")
-//    }
-//
-//    private fun hasParens(tokenPos: Int, tokens: List<Token>): Boolean {
-//        return (
-//            tokens[tokenPos + 1].getType() == OPENING_PARENS &&
-//                tokens[tokens.lastIndex - 1].getType() == CLOSING_PARENS
-//            )
-//    }
-//
-//    private fun isOperator(token: Token): Boolean {
-//        return when (token.getType()) {
-//            MINUS_OPERATOR -> true
-//            DIVIDE_OPERATOR -> true
-//            PLUS_OPERATOR -> true
-//            MULTIPLY_OPERATOR -> true
-//            else -> false
-//        }
-//    }
