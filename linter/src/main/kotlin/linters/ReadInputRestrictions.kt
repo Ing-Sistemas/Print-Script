a69c09933d92a052a9c3b8ddc0014783c902cc64
Felipe Germano <fgermano@mail.austral.edu.ar>
2024-09-11 00:04:08 -0300

    linter with 1.1

--- linter/src/main/kotlin/linters/ReadInputRestrictions.kt
+++ linter/src/main/kotlin/linters/ReadInputRestrictions.kt
@@ -7,6 +7,19 @@ import interfaces.Linter
 class ReadInputRestrictions(private val caseConfiguration: CaseConfiguration) : Linter {
     override fun lint(tokens: List<Token>): List<String> {
         val errors = mutableListOf<String>()
-        TODO("Not yet implemented")
+
+        tokens.forEach {token ->
+            if (token.getValue() == "readInput") {
+                val utilToken = tokens[tokens.indexOf(token) + 2]
+                if (utilToken.isLiteralOrIdentifier()) {
+                    errors.add("Cannot use readInput with ${utilToken.getValue()} in " +
+                        "line: ${utilToken.getPosition().getLine()} column: ${utilToken.getPosition().getColumn()}")
+                }
+            }
+        }
+        return errors
+    }
+    private fun Token.isLiteralOrIdentifier(): Boolean {
+        return this.getType().name == "LITERAL" || this.getType().name == "IDENTIFIER"
     }
 }
