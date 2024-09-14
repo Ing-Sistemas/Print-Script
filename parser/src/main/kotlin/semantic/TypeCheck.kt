8c8b215d58b6e586d541ce168dc549beb4fbeec7
= <aalma@mail.austral.edu.ar>
2024-09-14 01:15:41 -0300

    added comment for immutable considerations

--- parser/src/main/kotlin/semantic/TypeCheck.kt
+++ parser/src/main/kotlin/semantic/TypeCheck.kt
@@ -15,11 +15,15 @@ class TypeCheck(private val resultFactory: ResultFactory) {
         node: VariableDeclarationStatement,
         visitor: Visitor<ResultInformation>,
     ): ResultInformation {
-        val assignmentResult = node.getAssignmentExpression()?.accept(visitor)
-            ?: return resultFactory.createError("Assignment result is null")
+        val assignmentResult = node.getAssignmentExpression().accept(visitor)
 
         // todo if vardec.declarator is "const" construct is as immutable
         // todo then check in the assignment if the isMutable for allowance to change
+        // if (node.getDeclarator() == "const") llamar a una funcion cree un StoreValue inmutable
+        // hacerlo en un metodo distinto q llame directo a StorageManager.handleAssignment
+        // ver si esto hay q hacerlo antes de definir assignmentResult
+        // sino ver como pasarle q tiene q ser immutable a la hora de crearlo, de alguna forma
+        // le tiene q llegar hasta el StorageManager.handleAssignment
         if (assignmentResult.getErrors().isNotEmpty()) return assignmentResult
 
         val declarationNodeType = node.getTypeDeclarationExpression().getType()
