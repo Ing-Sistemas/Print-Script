f44a7b20d31a19487a5ee31929788582dafe813f
nicolasberretta <neberretta@mail.austral.edu.ar>
2024-09-15 11:55:29 -0300

    added maps to handle versions in Lexer

---
+++ lexer/src/main/kotlin/TokenPatternProvider.kt
@@ -0,0 +1,52 @@
+import org.example.token.TokenType
+import org.example.token.TokenType.*
+
+class TokenPatternProvider {
+    fun getPatterns(version: String): Map<TokenType, Regex>  {
+        val tokenPatterns: Map<TokenType, Regex> = when (version) {
+            "1.0" -> mapOf(
+                ASSIGNMENT to Regex("="),
+                SEMICOLON to Regex(";"),
+                COLON to Regex(":"),
+                KEYWORD to Regex("\\b(let)\\b"),
+                LITERAL_NUMBER to Regex("\\d+(\\.\\d+)?"),
+                LITERAL_STRING to Regex("\".*?\"|'.*?'"),
+                IDENTIFIER to Regex("[a-zA-Z_][a-zA-Z_0-9]*"),
+                STRING_TYPE to Regex("string"),
+                NUMBER_TYPE to Regex("number"),
+                PLUS_OPERATOR to Regex("\\+"),
+                MINUS_OPERATOR to Regex("-"),
+                MULTIPLY_OPERATOR to Regex("\\*"),
+                DIVIDE_OPERATOR to Regex("/"),
+                FUNCTION_CALL to Regex("\\b(println)\\b"),
+                OPENING_PARENS to Regex("\\("),
+                CLOSING_PARENS to Regex("\\)"),
+            )
+
+            "1.1" -> mapOf(
+                KEYWORD to Regex("\\b(let|const)\\b"),
+                IDENTIFIER to Regex("[a-zA-Z_][a-zA-Z0-9_]*"),
+                ASSIGNMENT to Regex("="),
+                SEMICOLON to Regex(";"),
+                COLON to Regex(":"),
+                LITERAL_NUMBER to Regex("\\d+(\\.\\d+)?"),
+                LITERAL_STRING to Regex("\".*?\"|'.*?'"),
+                STRING_TYPE to Regex("string"),
+                NUMBER_TYPE to Regex("number"),
+                PLUS_OPERATOR to Regex("\\+"),
+                MINUS_OPERATOR to Regex("-"),
+                MULTIPLY_OPERATOR to Regex("\\*"),
+                DIVIDE_OPERATOR to Regex("/"),
+                FUNCTION_CALL to Regex("\\b(if|else|println)\\b"),
+                OPENING_PARENS to Regex("\\("),
+                CLOSING_PARENS to Regex("\\)"),
+                OPENING_CURLY_BRACKS to Regex("\\{"),
+                CLOSING_CURLY_BRACKS to Regex("}"),
+                BOOLEAN_TYPE to Regex("\"boolean\""),
+                BOOLEAN to Regex("true|false"),
+            )
+            else -> throw IllegalArgumentException("Unsupported version: $version")
+        }
+        return tokenPatterns
+    }
+}
