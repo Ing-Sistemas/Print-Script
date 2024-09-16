0856b1b121e11b58c38c93276911cd203f12fc3f
nicolasberretta <neberretta@mail.austral.edu.ar>
2024-09-16 13:14:39 -0300

    added new test for lexer

--- lexer/src/test/kotlin/LexerTest.kt
+++ lexer/src/test/kotlin/LexerTest.kt
@@ -4,10 +4,19 @@ class LexerTest {
 
     @Test
     fun `Variable declaration statements`() {
-        val elements = JsonConverter().fromJson("1.0/golden/variable-declaration")
+        val elements = JsonConverter().fromJson("1.0/golden/variable_declaration")
         val result = Lexer("1.0").tokenize(TestingStringIterator(elements.input))
         val testResult = LexerTester().testResult(result, elements.expectedTokenTypes)
         if (testResult.message != "All tokens match the expected values.") throw Exception(testResult.message)
         assert(testResult.passed)
     }
+    @Test
+    fun `Println fun call statements`() {
+        val elements = JsonConverter().fromJson("1.0/golden/println_call")
+        val result = Lexer("1.0").tokenize(TestingStringIterator(elements.input))
+        val testResult = LexerTester().testResult(result, elements.expectedTokenTypes)
+        if (testResult.message != "All tokens match the expected values.") throw Exception(testResult.message)
+        assert(testResult.passed)
+    }
+
 }
