653bcb609df4f81353c49d4174b0a86728702431
nicolasberretta <neberretta@mail.austral.edu.ar>
2024-09-16 13:44:09 -0300

    added some 1.1 tests for cov

--- lexer/src/test/kotlin/LexerTest.kt
+++ lexer/src/test/kotlin/LexerTest.kt
@@ -3,18 +3,18 @@ import org.junit.jupiter.api.Test
 class LexerTest {
 
     @Test
-    fun `Variable declaration statements`() {
+    fun `Variable declaration statement`() {
         val elements = JsonConverter().fromJson("1.0/golden/variable_declaration")
-        val result = Lexer("1.0").tokenize(TestingStringIterator(elements.input))
+        val result = Lexer(elements.version).tokenize(TestingStringIterator(elements.input))
         val testResult = LexerTester().testResult(result, elements.expectedTokenTypes)
         if (testResult.message != "All tokens match the expected values.") throw Exception(testResult.message)
         assert(testResult.passed)
     }
 
     @Test
-    fun `Println fun call statements`() {
+    fun `Println fun call statement`() {
         val elements = JsonConverter().fromJson("1.0/golden/println_call")
-        val result = Lexer("1.0").tokenize(TestingStringIterator(elements.input))
+        val result = Lexer(elements.version).tokenize(TestingStringIterator(elements.input))
         val testResult = LexerTester().testResult(result, elements.expectedTokenTypes)
         if (testResult.message != "All tokens match the expected values.") throw Exception(testResult.message)
         assert(testResult.passed)
@@ -23,7 +23,25 @@ class LexerTest {
     @Test
     fun `Binary Expression`() {
         val elements = JsonConverter().fromJson("1.0/golden/binary_expression")
-        val result = Lexer("1.0").tokenize(TestingStringIterator(elements.input))
+        val result = Lexer(elements.version).tokenize(TestingStringIterator(elements.input))
+        val testResult = LexerTester().testResult(result, elements.expectedTokenTypes)
+        if (testResult.message != "All tokens match the expected values.") throw Exception(testResult.message)
+        assert(testResult.passed)
+    }
+
+
+    @Test
+    fun `If function call statement`() {
+        val elements = JsonConverter().fromJson("1.1/golden/if_function_call")
+        val result = Lexer(elements.version).tokenize(TestingStringIterator(elements.input))
+        val testResult = LexerTester().testResult(result, elements.expectedTokenTypes)
+        if (testResult.message != "All tokens match the expected values.") throw Exception(testResult.message)
+        assert(testResult.passed)
+    }
+    @Test
+    fun `Boolean variable declaration statement`() {
+        val elements = JsonConverter().fromJson("1.1/golden/if_function_call")
+        val result = Lexer(elements.version).tokenize(TestingStringIterator(elements.input))
         val testResult = LexerTester().testResult(result, elements.expectedTokenTypes)
         if (testResult.message != "All tokens match the expected values.") throw Exception(testResult.message)
         assert(testResult.passed)
