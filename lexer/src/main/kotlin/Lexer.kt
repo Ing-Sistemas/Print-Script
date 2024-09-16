505e542ea045bfa7105ab9b3d33ca1c43262fd9c
nicolasberretta <neberretta@mail.austral.edu.ar>
2024-09-15 23:34:54 -0300

    changed lexer, now it pulls lines

--- lexer/src/main/kotlin/Lexer.kt
+++ lexer/src/main/kotlin/Lexer.kt
@@ -6,53 +6,45 @@ class Lexer(
     private val tokenPatterns: Map<TokenType, Regex> by lazy {
         TokenPatternProvider().getPatterns(version)
     }
-    fun tokenize(input: String): Iterator<Token> {
-        return object : Iterator<Token> {
-            var currentIndex = 0
-            var lineNumber = 1
+    private var currentLine: String? = null
+    private var currentIndex = 0
+    private var lineNumber = 1
 
+    fun tokenize(input: Iterator<String>): Iterator<Token> {
+        return object : Iterator<Token> {
             override fun hasNext(): Boolean {
-                return currentIndex < input.length
+                while (currentLine == null || currentIndex >= currentLine!!.length) {
+                    if (input.hasNext()) {
+                        currentLine = input.next()
+                        currentIndex = 0
+                        lineNumber++
+                    } else {
+                        return false
+                    }
+                }
+                return currentLine != null
             }
             override fun next(): Token {
-                if (!hasNext()) {
-                    throw NoSuchElementException()
-                }
-                val token = tokenizeLine(input)
-                if (token != null) {
-                    return token
-                } else {
-                    throw NoSuchElementException()
-                }
-            }
-
-            private fun tokenizeLine(input: String): Token? {
-                while (currentIndex < input.length) {
-                    for ((tokenType, regex) in tokenPatterns) {
-                        val matcher = regex.find(input, currentIndex)
-                        if (matcher != null && matcher.range.first == currentIndex) {
-                            val tokenValue = matcher.value
-                            val tokenPosition = Position(currentIndex + 1, lineNumber)
-                            lineNumber += tokenValue.count { it == '\n' }
-                            currentIndex += tokenValue.length
-                            return Token(
-                                tokenType,
-                                tokenValue,
-                                tokenPosition
-                            )
+                if (!hasNext()) throw NoSuchElementException()
+                currentLine?.let { line ->
+                    while (currentIndex < line.length) {
+                        for ((tokenType, regex) in tokenPatterns) {
+                            val matcher = regex.find(line, currentIndex)
+                            if (matcher != null && matcher.range.first == currentIndex) {
+                                val tokenValue = matcher.value
+                                val tokenPosition = Position(currentIndex + 1, lineNumber)
+                                currentIndex += tokenValue.length
+                                return Token(tokenType, tokenValue, tokenPosition)
+                            }
+                        }
+                        if (line[currentIndex].isWhitespace()) {
+                            currentIndex++
+                        } else {
+                            throw Exception("Unexpected character '${line[currentIndex]}' at line: $lineNumber, column: ${currentIndex + 1}")
                         }
                     }
-
-                    if (input[currentIndex].isWhitespace()) {
-                        if (input[currentIndex] == '\n') {
-                            lineNumber++
-                        }
-                        currentIndex++
-                    } else {
-                        throw Exception("Unexpected character '${input[currentIndex]}' at line: $lineNumber, column: ${currentIndex + 1}")
-                    }
                 }
-                return null
+                throw NoSuchElementException("No token found")
             }
         }
     }
