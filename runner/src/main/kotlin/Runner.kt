602e83190adb6baae9b8cffd1dd9bda066b5ce86
Felipe Germano <fgermano@mail.austral.edu.ar>
2024-10-01 20:21:31 -0300

    aborted lists in interpreter im dword

--- runner/src/main/kotlin/Runner.kt
+++ runner/src/main/kotlin/Runner.kt
@@ -3,7 +3,10 @@ package com.printscript.runner
 import com.printscript.interpreter.Interpreter
 import com.printscript.interpreter.interfaces.EnvProvider
 import com.printscript.interpreter.interfaces.InputProvider
+import com.printscript.interpreter.interfaces.InterpreterResult
 import com.printscript.interpreter.interfaces.OutPutProvider
+import com.printscript.interpreter.results.InterpreterFailure
+import com.printscript.interpreter.results.InterpreterSuccess
 import com.printscript.interpreter.utils.Storage
 import com.printscript.lexer.Lexer
 import com.printscript.parser.ASTIterator
@@ -20,14 +23,14 @@ class Runner(
     private val versions = setOf("1.0", "1.1")
     private val interpreter = Interpreter(outPutProvider, inputProvider, envProvider)
 
-    fun run(inputStream: InputStream, version: String): List<String> {
+    fun run(inputStream: InputStream, version: String): InterpreterResult {
         if (version !in versions) {
             throw IllegalArgumentException("version: '$version' is not supported")
         }
 
         val readerIterator = ReaderIterator().getLineIterator(inputStream)
         val tokens = Lexer(version).tokenize(readerIterator)
-        var result = emptyList<String>()
+        var result: InterpreterResult = InterpreterFailure("")
         val astIterator = ASTIterator(tokens, parser)
         while (astIterator.hasNext()) {
             val ast = astIterator.next()
