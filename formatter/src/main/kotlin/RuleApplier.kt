e42886739af81ce41ca28cb7aa3603fad5810e35
agussoul <aalma@mail.austral.edu.ar>
2024-10-22 13:03:56 -0300

    rm comments y fix command config/config

--- formatter/src/main/kotlin/RuleApplier.kt
+++ formatter/src/main/kotlin/RuleApplier.kt
@@ -22,12 +22,6 @@ class RuleApplier(private val config: FormatterConfig, private val builder: Stri
                     builder.append(code)
                 }
             }
-//            "println" -> {
-//                if (config.lineJumpBeforePrintln > 0) {
-//                    LineJumpBeforePrintln(config.lineJumpBeforePrintln).apply(code, config, builder)
-//                }
-//                builder.append(code)
-//            }
             "+", "-", "*", "/" -> {
                 if (config.spaceAroundOperators) {
                     SpaceAroundOperator().apply(code, config, builder)
@@ -35,13 +29,6 @@ class RuleApplier(private val config: FormatterConfig, private val builder: Stri
                     builder.append(code)
                 }
             }
-//            ";" -> {
-//                if (config.lineJumpAfterSemicolon) {
-//                    LineJumpAfterSemicolon().apply(code, config, builder)
-//                }
-//                builder.append(code)
-//            }
-
             else -> builder.append(code)
         }
     }
