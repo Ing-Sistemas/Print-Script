27b00a5115d7a839f75f364b15e60a1e709b0a08
nicolasberretta <neberretta@mail.austral.edu.ar>
2024-10-23 23:45:30 -0300

    changed files to inputStreams

--- cli/src/main/kotlin/logic/AnalyzeLogic.kt
+++ cli/src/main/kotlin/logic/AnalyzeLogic.kt
@@ -6,13 +6,14 @@ import com.printscript.linter.linters.StaticCodeAnalyzer
 import com.printscript.runner.ReaderIterator
 import com.printscript.token.Token
 import java.io.File
+import java.io.InputStream
 
 class AnalyzeLogic {
-    fun analyse(version: String, linterConfig: String, configFile: File, inputFile: File): List<String> {
+    fun analyse(version: String, input: InputStream, configFile: File): List<String> {
         val tokens = mutableListOf<Token>()
         val config = ConfigLoader.loadConfiguration(configFile.path)
         val linter = StaticCodeAnalyzer(config, version)
-        val readerIterator = ReaderIterator().getLineIterator(inputFile.inputStream())
+        val readerIterator = ReaderIterator().getLineIterator(input)
         val tokenIterator = Lexer(version).tokenize(readerIterator)
 
         while (tokenIterator.hasNext()) {
--- cli/src/main/kotlin/logic/ValidateLogic.kt
+++ cli/src/main/kotlin/logic/ValidateLogic.kt
@@ -4,11 +4,11 @@ import com.printscript.ast.ASTNode
 import com.printscript.lexer.Lexer
 import com.printscript.parser.Parser
 import com.printscript.runner.ReaderIterator
-import java.io.File
+import java.io.InputStream
 
 class ValidateLogic {
-    fun validate(version: String, inputFile: File): ASTNode {
-        val readerIterator = ReaderIterator().getLineIterator(inputFile.inputStream())
+    fun validate(version: String, inputFile: InputStream): ASTNode {
+        val readerIterator = ReaderIterator().getLineIterator(inputFile)
         val tokens = Lexer(version).tokenize(readerIterator)
         return Parser().parse(tokens)
     }
