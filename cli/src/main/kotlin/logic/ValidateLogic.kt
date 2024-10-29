0c33b3b6253d24e684ea05599d84d35ec832b6e5
nicolasberretta <neberretta@mail.austral.edu.ar>
2024-10-29 16:58:08 -0300

    fixed cli (rookie mistake sorry)

--- cli/src/main/kotlin/logic/ValidateLogic.kt
+++ cli/src/main/kotlin/logic/ValidateLogic.kt
@@ -1,15 +1,18 @@
 package com.printscript.cli.logic
 
-import com.printscript.ast.ASTNode
 import com.printscript.lexer.Lexer
+import com.printscript.parser.ASTIterator
 import com.printscript.parser.Parser
 import com.printscript.runner.ReaderIterator
 import java.io.InputStream
 
 class ValidateLogic {
-    fun validate(version: String, inputFile: InputStream): ASTNode {
+    fun validate(version: String, inputFile: InputStream) {
         val readerIterator = ReaderIterator().getLineIterator(inputFile)
         val tokens = Lexer(version).tokenize(readerIterator)
-        return Parser().parse(tokens)
+        val astIterator = ASTIterator(tokens, Parser())
+        while (astIterator.hasNext()) {
+            astIterator.next()
+        }
     }
 }
